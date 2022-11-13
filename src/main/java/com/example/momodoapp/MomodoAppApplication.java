16ee1400d329241e2e4db5d25bec418182f03772
Lasse Rasmussen <lass531n@stud.kea.dk>
2022-11-13 12:29:11 +0100

    first commit

---
+++ src/main/java/com/example/momodoapp/MomodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.momodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/momodoapp/MomodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.momodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
